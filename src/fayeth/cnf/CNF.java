3a98e5586efca74c90f03f99f290231c1b193801
Domenico Fabio Marino <nospamdomi@hotmail.it>
2017-11-11 10:03:32 +0000

    Initial CNF parsing and writing
    Signed-off-by: Domenico Fabio Marino <nospamdomi@hotmail.it>
    
    Delete test files
    
    Signed-off-by: Domenico Fabio Marino <nospamdomi@hotmail.it>

---
+++ src/fayeth/cnf/CNF.java
@@ -0,0 +1,105 @@
+package fayeth.cnf;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CNF {
+    private final List<List<Integer>> clauses;
+    private final Set<Integer> literals;
+    private static final Pattern CNF_PATTERN = Pattern.compile("p cnf (\\d+) (\\d+)");
+
+    private CNF(List<List<Integer>> clauses, Set<Integer> literals) {
+        this.clauses = clauses;
+        this.literals = literals;
+    }
+
+    public Set<Integer> getLiterals() {
+        return literals;
+    }
+
+    public List<List<Integer>> getClauses() {
+        return clauses;
+    }
+
+    @Override
+    public String toString() {
+        return "CNF{" +
+                "clauses=" + clauses +
+                ", literals=" + literals +
+                '}';
+    }
+
+    /**
+     * Initialises a new CNF formula from a file
+     * @param path path to the file that contains the formula
+     * @return a CNF class representing the formula contained inside the file
+     * @throws IOException if the file does not exist
+     * @throws RuntimeException if the file is not in CNF format
+     */
+    public static CNF fromFile(String path) throws IOException {
+        final File f = new File(path);
+        if(!f.exists()) {
+            throw new FileNotFoundException(path + " does not exist");
+        }
+        final BufferedReader bi = new BufferedReader(new FileReader(f));
+        String line;;
+
+        // Skip all the initial comments that some queries have
+        while((line = bi.readLine()) != null) {
+            if(line.startsWith("c")) continue;
+            if(line.startsWith("p")) {
+                break;
+            }
+        }
+        if(line == null) {
+            bi.close();
+            throw new IllegalArgumentException("File did not contain a valid CNF formula");
+        }
+        Matcher m = CNF_PATTERN.matcher(line);
+        if(!m.matches()) {
+            bi.close();
+            throw new IllegalArgumentException("File is not in CNF format: " + line);
+        }
+        final Integer numVariables = Integer.parseInt(m.group(1));
+        final Integer numClauses = Integer.parseInt(m.group(2));
+        final List<List<Integer>> clauses = new ArrayList<>(numClauses);
+        final Set<Integer> literals = new HashSet<>(numVariables);
+
+        while((line = bi.readLine()) != null) {
+            if(line.startsWith("c")) continue;
+            List<Integer> clause = new ArrayList<>();
+            String[] splitSep = line.split("\\s");
+            for(String s : splitSep) {
+                if(s.equals("0"))
+                    break;
+                Integer l = Integer.parseInt(s);
+                clause.add(l);
+                literals.add(Math.abs(l));
+            }
+            clauses.add(clause);
+        }
+        bi.close();
+        return new CNF(clauses, literals);
+    }
+
+    /**
+     * Creates a file from a CNF
+     * @param file path to the output file
+     * @throws IOException according to BufferedWriter
+     */
+    public void toFile(String file) throws IOException {
+        File f = new File(file);
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        bw.append(String.format("p cnf %d %d\n", literals.size(), clauses.size()));
+        for(List<Integer> clause : clauses) {
+            for(Integer i : clause) {
+                bw.append(i + " ");
+            }
+            bw.append("0\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
